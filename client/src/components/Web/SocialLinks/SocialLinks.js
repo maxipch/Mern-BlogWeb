b295de5df10c30b849d4602f148c238fa025b06e
maxHuB <maximilianogordillo@hotmail.com>
2021-05-07 16:23:10 -0300

    commit

--- client/src/components/Web/SocialLinks/SocialLinks.js
+++ client/src/components/Web/SocialLinks/SocialLinks.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { ReactComponent as YouTubeIcon } from "../../../assets/img/svg/youtube.svg";
+import { ReactComponent as TwitterIcon } from "../../../assets/img/svg/twitter.svg";
+import { ReactComponent as LinkedinIcon } from "../../../assets/img/svg/linkedin.svg";
+import { ReactComponent as FacebookIcon } from "../../../assets/img/svg/facebook.svg";
+
+import "./SocialLinks.scss";
 
 export default function SocialLinks(){
-    return <p>SocialMedia</p>
+    return (
+        <div className="social-link">
+            <a 
+                href="https://youtube.com"
+                className="youtube"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <YouTubeIcon/>
+            </a>
+
+            <a
+                href="www.twiter.com"
+                className="twitter"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <TwitterIcon/>
+            </a>
+
+            <a
+                href="www.facebook.com"
+                className="facebook"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <FacebookIcon/>
+            </a>
+
+            <a
+                href="www.linkedin.com"
+                className="linkedin"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <LinkedinIcon/>
+            </a>
+        </div>
+    );
 }

2b35f1f1a47369983f3374831b4cd16aceae7b5b
maxHuB <maximilianogordillo@hotmail.com>
2021-05-07 15:50:03 -0300

    Version 1.1

--- client/src/components/Web/MenuTop/MenuTop.js
+++ client/src/components/Web/MenuTop/MenuTop.js
@@ -1,26 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
+import { getMenuApi } from "../../../api/menu";
 import Logo from "../../../assets/img/svg/Logo-design.svg";
 
 import "./MenuTop.scss";
 
 export default function MenuTop(){
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+        getMenuApi().then(response => {
+            const arrayMenu = [];
+            response.menu.forEach(item => {
+                item.active && arrayMenu.push(item);
+            })
+            setMenuData(arrayMenu);
+        })
+    }, []);
     return (
-        <Menu className="menu-top" mode="horizontal">
-            <Menu.Item className="menu-top__logo">
+        <Menu className="menu-top-web" mode="horizontal">
+            <Menu.Item className="menu-top-web__logo">
                 <Link to={"/"}>
                     <img src={Logo} alt="Logo"/>
                 </Link>
             </Menu.Item>
 
-            <Menu.Item className="menu-top__item">
-                <Link to={"/"}>Home</Link>
-            </Menu.Item>
+            {menuData.map(item => {
+                const external = item.url.indexOf("http") > -1 ? true : false;
 
-            <Menu.Item className="menu-top__item">
-                <Link to={"/contact"}>Contacto</Link>
-            </Menu.Item>
+                if(external){
+                    return (
+                        <Menu.Item key={item._id} className="menu-top-web__item">
+                            <a href={item.url} target="_blank" rel="noreferrer">{item.title}</a>
+                        </Menu.Item>
+                    );
+                }
+
+                return(
+                    <Menu.Item key={item._id} className="menu-top-web__item">
+                        <Link to={item.url}>{item.title}</Link>
+                    </Menu.Item>
+                );
+            })}
 
             <div>Social Media</div>
         </Menu>
